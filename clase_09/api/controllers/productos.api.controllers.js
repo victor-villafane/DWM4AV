c1db6781e7e2f18cfd01b11697320d5b413e7489
victor-villafane <victor.villafane@davinci.edu.ar>
2026-04-16 10:45:22 -0300

    clase_09

---
+++ clase_09/api/controllers/productos.api.controllers.js
@@ -0,0 +1,74 @@
+import * as service from "../../services/productos.services.js"
+
+export async function getProductos(req, res) {
+    const filtros = req.query
+    try {
+        const productos = await service.getProductos(filtros)
+        res.status(200).json(productos)
+    } catch (error) {
+        res.status(500).json({ message: "Error al traer los productos" })
+    }
+}
+
+export async function getProductosById(req, res) {
+    const id = req.params.id
+    try {
+        const producto = await service.getProductosById(id)
+        res.status(200).json(producto)
+    } catch (error) {
+        res.status(500).json({ message: "Error al traer los productos" })
+    }
+}
+
+export async function saveProducto(req, res) {
+    try {
+        const producto = {
+            nombre: req.body.nombre,
+            precio: req.body.precio
+        }
+        const respuesta = await service.guardarProducto(producto)
+        res.status(201).json(respuesta)
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+}
+
+export async function reemplazarProducto(req, res) {
+    const id = req.params.id
+    try {
+        const producto = {
+            id: id,
+            nombre: req.body.nombre,
+            precio: req.body.precio
+        }
+        const respuesta = await service.editarProducto(producto)
+        res.status(202).json(respuesta)
+    } catch (error) {
+        res.status(500).json({ message: "Error guardar el producto" })
+    }
+}
+
+export async function actualizarProducto(req, res){
+    const id = req.params.id
+    try {
+        const productoAntiguo = await service.getProductosById(id)
+        const producto = {}
+        producto.nombre = req.body?.nombre ? req.body.nombre : productoAntiguo?.nombre
+        producto.precio = req.body?.precio ? req.body.precio : productoAntiguo?.precio
+        producto.id = id
+        const respuesta = await service.editarProducto(producto)
+        res.status(202).json(respuesta)
+    } catch (error) {
+        res.status(500).json({ message: "Error guardar el producto" })
+    }    
+}
+
+export async function borrarProducto(req, res){
+    const id = req.params.id
+    try {
+        const respuesta = await service.borrarProducto(id)
+        res.status(202).json(respuesta)
+    } catch (error) {
+        res.status(500).json({ message: "Error guardar el producto" })
+    }
+}
