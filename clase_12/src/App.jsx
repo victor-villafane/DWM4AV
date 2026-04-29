cdf72f8493210e12b8b6419b2f5b33bf381725ef
victor-villafane <victor.villafane@davinci.edu.ar>
2026-04-29 12:57:49 -0300

    clase_12

---
+++ clase_12/src/App.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+
+// export default class App extends React.Component{
+//   render(){
+//     return ( <div>Hola!</div> )
+//   }
+// }
+
+
+// export default function App(){
+//   console.log("Hola")
+//   return ( //Lo pueden pensar como HTML -> jsx
+//     <div>
+//       Hola!
+//     </div>
+//   )
+// }
+
+const App = () => {
+  // ------------
+  // TODO EL CODIGO JS QUE QUIERAN!
+  // ------------
+  const variable = "Hola desde js"
+  return (
+    <div className="" >
+      { variable }
+    </div>
+  )
+}
+
+export default App
