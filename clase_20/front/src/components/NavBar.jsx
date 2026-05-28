173f665a8b37bb8677014c304bd4e60096b85e94
victor-villafane <victor.villafane@davinci.edu.ar>
2026-05-28 10:13:49 -0300

    clase_20

---
+++ clase_20/front/src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router"
+
+const NavBar = () => {
+    // TODO Esto deberia ser un estado
+    const session = JSON.parse(localStorage.getItem("session"))
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        {
+                            session
+                                ? <li className="nav-item">
+                                    <Link className="nav-link" to="/logout">Logout</Link>
+                                </li>
+                                : <li className="nav-item">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                        }
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ clase_20/front/src/routes/Router.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router";
+import { lazy, Suspense } from "react"
+import Layout from "../components/Layout";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+const Login = lazy(() => import('../pages/Login'))
+const Logout = lazy(() => import('../pages/Logout'))
+const Pokemons = lazy(() => import('../pages/Pokemons'))
+const Detail = lazy(() => import('../pages/Detail'))
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <ProtectedRoute element={<Pokemons />} />,
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/detail/:idPokemon",
+        element: <ProtectedRoute element={<Detail />} />
+      },
+      {
+        path: "/logout",
+        element: <Logout />
+      }
+    ]
+  }
+]);
+
+export default router
